2be834d425ece3ac8c86debeddf8cb9a33e5dd68
yaozm <ityaozm@gmail.com>
2021-12-18 00:04:49 +0800

    Add support for `有道`

--- examples/main.go
+++ examples/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/guanguans/translator/baidu"
+	"github.com/guanguans/translator/youdao"
 )
 
 func main() {
 	baidu := baidu.New("appid", "appSecret")
-
-    // (*resty.Response)(0xc00008e0f0)({"from":"zh","to":"en","trans_result":[{"src":"\u4f60\u597d","dst":"Hello"}]})
-    // (interface {}) <nil>
 	spew.Dump(baidu.Translate("你好", "zh", "en"))
+
+	youdao := youdao.New("appid", "appSecret")
+	spew.Dump(youdao.Translate("你好", "zh-CHS", "en"))
 }
